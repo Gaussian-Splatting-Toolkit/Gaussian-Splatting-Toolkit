9aeba8c29e52ad9eac176ee873d6220093822115
Hu Tianrun <tianrunhu@gmail.com>
2024-01-10 21:15:34 +0800

    data pre-processing for colmap runnable

---
+++ gs_toolkit/data/process_data/base_converter_to_gstk_dataset.py
@@ -0,0 +1,44 @@
+import os
+from gs_toolkit.utils.rich_utils import CONSOLE
+from abc import abstractmethod
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass
+class BaseConverterToGstkDataset():
+    """Base class to process images or video into a gs_toolkit dataset."""
+    
+    input: Path
+    """Path to the data, either a video or a folder with images."""
+    data_dir: Path
+    """Path to the output directory."""
+    eval_data: Path | None = None
+    """Path to the evaluation data, either a video or a folder with images. If set to None, the first will be used for both training and evaluation."""
+    verbose: bool = False
+    """If True, print more information."""
+    
+    def __post_init__(self) -> None:
+        self.data_dir.mkdir(parents=True, exist_ok=True)
+        self.input_dir.mkdir(parents=True, exist_ok=True)
+        
+    def extract_keyframes(self, fps: int) -> None:
+        """Extract keyframes from the input video.
+        
+        Args:
+            fps: The number of frames per second to extract.
+        """
+        # Process data with ffmpeg and save the results into input dir.
+        with CONSOLE.status("Extracting keyframes...", spinner="bouncingBall"):
+            os.system(
+                f"ffmpeg -i {self.input} -vf fps={fps} {self.input_dir}/%06d.png"
+            )
+        
+    @property
+    def input_dir(self) -> Path:
+        """Path to the directory containing the images."""
+        return self.data_dir / "input"
+    
+    @abstractmethod
+    def main(self) -> None:
+        """This method implements the conversion logic for each type of data"""
+        raise NotImplementedError("The main method must be implemented.")
---
+++ gs_toolkit/data/process_data/colmap_converter_to_gstk_dataset.py
@@ -0,0 +1,186 @@
+import os
+from dataclasses import dataclass
+import shutil
+from gs_toolkit.utils.rich_utils import CONSOLE
+from gs_toolkit.data.process_data.base_converter_to_gstk_dataset import (
+    BaseConverterToGstkDataset,
+)
+
+
+@dataclass
+class ColmapConverterToGstkDataset(BaseConverterToGstkDataset):
+    """Process data using Colmap.
+
+    This script does the following:
+
+    1. Select keyframes from the input video.
+    2. Extract the pose and sparse point cloud using COLMAP.
+    """
+
+    fps: int = 3
+    """Number of frames per second to extract from the video."""
+    colmap_command: str = "colmap"
+    """Path to the colmap executable."""
+    magick_command: str = "magick"
+    """Path to the magick executable."""
+    camera: str = "OPENCV"
+    """Camera model to use for colmap."""
+    no_gpu: bool = False
+    """If True, disable GPU usage."""
+    skip_matching: bool = False
+    """If True, skip feature matching"""
+    resize: bool = False
+    """If True, resize images to 50%, 25% and 12.5% of the original size."""
+
+    def main(self) -> None:
+        """Main function."""
+        summary_log = []
+
+        # If the data is a video, extract the keyframes.
+        if self.input.suffix in [".mp4", ".avi", ".mov", ".MP4", ".MOV"]:
+            self.extract_keyframes(self.fps)
+
+        use_gpu = 1 if not self.no_gpu else 0
+        
+        data_dir_str = str(self.data_dir)
+
+        if not self.skip_matching:
+            os.makedirs(data_dir_str + "/distorted/sparse", exist_ok=True)
+
+            ## Feature extraction
+            feat_extracton_cmd = (
+                self.colmap_command + " feature_extractor "
+                "--database_path "
+                + data_dir_str
+                + "/distorted/database.db \
+                --image_path "
+                + data_dir_str
+                + "/input \
+                --ImageReader.single_camera 1 \
+                --ImageReader.camera_model "
+                + self.camera
+                + " \
+                --SiftExtraction.use_gpu "
+                + str(use_gpu)
+            )
+            exit_code = os.system(feat_extracton_cmd)
+            if exit_code != 0:
+                CONSOLE.log(
+                    f"Feature extraction failed with code {exit_code}. Exiting."
+                )
+                exit(exit_code)
+
+            ## Feature matching
+            feat_matching_cmd = (
+                self.colmap_command
+                + " exhaustive_matcher \
+                --database_path "
+                + data_dir_str
+                + "/distorted/database.db \
+                --SiftMatching.use_gpu "
+                + str(use_gpu)
+            )
+            exit_code = os.system(feat_matching_cmd)
+            if exit_code != 0:
+                CONSOLE.log(f"Feature matching failed with code {exit_code}. Exiting.")
+                exit(exit_code)
+
+            ### Bundle adjustment
+            # The default Mapper tolerance is unnecessarily large,
+            # decreasing it speeds up bundle adjustment steps.
+            mapper_cmd = (
+                self.colmap_command
+                + " mapper \
+                --database_path "
+                + data_dir_str
+                + "/distorted/database.db \
+                --image_path "
+                + data_dir_str
+                + "/input \
+                --output_path "
+                + data_dir_str
+                + "/distorted/sparse \
+                --Mapper.ba_global_function_tolerance=0.000001"
+            )
+            exit_code = os.system(mapper_cmd)
+            if exit_code != 0:
+                CONSOLE.log(f"Mapper failed with code {exit_code}. Exiting.")
+                exit(exit_code)
+
+        ### Image undistortion
+        ## We need to undistort our images into ideal pinhole intrinsics.
+        img_undist_cmd = (
+            self.colmap_command
+            + " image_undistorter \
+            --image_path "
+            + data_dir_str
+            + "/input \
+            --input_path "
+            + data_dir_str
+            + "/distorted/sparse/0 \
+            --output_path "
+            + data_dir_str
+            + "\
+            --output_type COLMAP"
+        )
+        exit_code = os.system(img_undist_cmd)
+        if exit_code != 0:
+            CONSOLE.log(f"Mapper failed with code {exit_code}. Exiting.")
+            exit(exit_code)
+
+        files = os.listdir(data_dir_str + "/sparse")
+        os.makedirs(data_dir_str + "/sparse/0", exist_ok=True)
+        # Copy each file from the source directory to the destination directory
+        for file in files:
+            if file == "0":
+                continue
+            source_file = os.path.join(data_dir_str, "sparse", file)
+            destination_file = os.path.join(data_dir_str, "sparse", "0", file)
+            shutil.move(source_file, destination_file)
+
+        if self.resize:
+            print("Copying and resizing...")
+
+            # Resize images.
+            os.makedirs(data_dir_str + "/images_2", exist_ok=True)
+            os.makedirs(data_dir_str + "/images_4", exist_ok=True)
+            os.makedirs(data_dir_str + "/images_8", exist_ok=True)
+            # Get the list of files in the source directory
+            files = os.listdir(data_dir_str + "/images")
+            # Copy each file from the source directory to the destination directory
+            for file in files:
+                source_file = os.path.join(data_dir_str, "images", file)
+
+                destination_file = os.path.join(data_dir_str, "images_2", file)
+                shutil.copy2(source_file, destination_file)
+                exit_code = os.system(
+                    self.magick_command + " mogrify -resize 50% " + destination_file
+                )
+                if exit_code != 0:
+                    CONSOLE.log(f"50% resize failed with code {exit_code}. Exiting.")
+                    exit(exit_code)
+
+                destination_file = os.path.join(data_dir_str, "images_4", file)
+                shutil.copy2(source_file, destination_file)
+                exit_code = os.system(
+                    self.magick_command + " mogrify -resize 25% " + destination_file
+                )
+                if exit_code != 0:
+                    CONSOLE.log(f"25% resize failed with code {exit_code}. Exiting.")
+                    exit(exit_code)
+
+                destination_file = os.path.join(data_dir_str, "images_8", file)
+                shutil.copy2(source_file, destination_file)
+                exit_code = os.system(
+                    self.magick_command + " mogrify -resize 12.5% " + destination_file
+                )
+                if exit_code != 0:
+                    CONSOLE.log(f"12.5% resize failed with code {exit_code}. Exiting.")
+                    exit(exit_code)
+
+        summary_log.append("Colmap processing complete.")
+
+        CONSOLE.rule("[bold green]:tada: :tada: :tada: All DONE :tada: :tada: :tada:")
+        for summary in summary_log:
+            CONSOLE.print(summary, justify="center")
+        CONSOLE.rule()
