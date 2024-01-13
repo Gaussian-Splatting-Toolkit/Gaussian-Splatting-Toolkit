49a5260a74e3b31549337446d8464f615d1992ba
Hu Tianrun <tianrunhu@gmail.com>
2024-01-13 09:56:55 +0800

    clean the code

--- gs_toolkit/field_components/__init__.py
+++
@@ -1,5 +0,0 @@
-"""init field modules"""
-from .base_field_component import FieldComponent as FieldComponent
-from .encodings import Encoding as Encoding
-from .encodings import ScalingAndOffset as ScalingAndOffset
-from .mlp import MLP as MLP
