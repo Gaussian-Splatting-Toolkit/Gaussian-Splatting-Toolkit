ec875047c4643082ff9d727eaaad955311e538ee
Hu Tianrun <tianrunhu@gmail.com>
2024-05-01 09:43:28 +0800

    init doc

--- docs/conf.py
+++ docs/conf.py
@@ -6,9 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "Gaussian Splatting Toolkit"
+project = "GSTK"
 copyright = "2024, Hu Tianrun"
 author = "Hu Tianrun"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -16,11 +17,11 @@ author = "Hu Tianrun"
 extensions = []
 
 templates_path = ["_templates"]
-exclude_patterns = []
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "alabaster"
 html_static_path = ["_static"]
---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. GSTK documentation master file, created by
+   sphinx-quickstart on Wed May  1 09:41:24 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to GSTK's documentation!
+================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
